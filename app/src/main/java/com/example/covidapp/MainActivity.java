ff7eed16ec425055c06753e5030acf0faeb7c984
Caroline Rosa <cpauladarosa@gmail.com>
2021-06-20 17:27:07 -0300

    new icon

--- app/src/main/java/com/example/covidapp/MainActivity.java
+++ app/src/main/java/com/example/covidapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.covid_app);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+        setTitle(getResources().getString(R.string.app_name));
     }
 }
